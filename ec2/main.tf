348a3664d8738bef0bf1df970cd41169c2f318c5
Abraham Leal <abraham.carlos.leal@icloud.com>
2023-07-15 15:43:04 -0400

    Add eip to ec2 project

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "ec2" {
   ami = "ami-06ca3ca175f37dd66"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "elasticeip" {
+  instance = aws_instance.ec2.id
+}
+
+output "EIP" {
+  value = aws_eip.elasticeip.public_ip
+}
